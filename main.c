372bc0ca145ae3dd1b1bc0b8c9ac158fc21504be
D E Costa <d.ethan.costa@gmail.com>
2023-11-08 16:49:40 -0500

    Complete challenge

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <wchar.h>
 #include <wctype.h>
 
@@ -12,15 +13,46 @@ int count_chars(FILE *file);
 int count_words(FILE *file);
 
 int main(int argc, char *argv[]) {
-	if (argc < 2) {
-		printf("Please provide a file name.\n");
-		return EXIT_FAILURE;
+	int opt;
+
+	int line_opt = 0;
+	int word_opt = 0;
+	int char_opt = 0;
+	char *filename = NULL;
+
+	while ((opt = getopt(argc, argv, "lmw")) != -1) {
+		switch(opt) {
+			case 'l':
+				line_opt = 1;
+				break;
+			case 'm':
+				char_opt = 1;
+				break;
+			case 'w':
+				word_opt = 1;
+				break;
+			case '?':
+				printf("Unknown flag: %c\n", optopt);
+				return EXIT_FAILURE;
+		}
+	} 
+	for (; optind < argc; optind++) {
+		if (filename != NULL) {
+			printf("Too many arguments: should only have file name.\n");
+			return EXIT_FAILURE;
+		}
+		filename = argv[optind];
 	}
+
 	int word_count = 0;
 	int line_count = 0;
 	int char_count = 0;
-	char *filename = argv[argc-1]; // Assumes that filename is last argument given
-	FILE *f = fopen(filename, "r"); 
+	FILE *f;
+	if (filename == NULL) {
+		f = stdin;
+	} else {
+		f = fopen(filename, "r"); 
+	}
 	if (f == NULL) {
 		printf("Error opening file\n");
 		return EXIT_FAILURE;
@@ -42,16 +74,40 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	if (argc == 3 && strcmp(argv[1], "-l") == 0) {
+	if (filename == NULL) {
+		filename = "";
+	}
+
+	printf("\t");
+	/*
+	if (line_opt) {
 		printf("%d %s\n", line_count, filename);
-	} else if (argc == 3 && strcmp(argv[1], "-m") == 0) {
+	} else if (char_opt) {
 		printf("%d %s\n", char_count, filename);
-	} else if (argc == 3 && strcmp(argv[1], "-w") == 0) {
+	} else if (word_opt) {
 		printf("%d %s\n", word_count, filename);
-	} else if (argc == 2) {
+	} else if (!word_opt && !line_opt && !char_opt) {
 		printf("%d %d %d %s\n", line_count, word_count, char_count, filename);
 	}
-	fclose(f);
+	*/
+
+	if (line_opt) {
+		printf("%d", line_count);
+	}
+	if (word_opt) {
+		printf(" %d", word_count);
+	}
+	if (char_opt) {
+		printf(" %d", char_count);
+	}
+	if (!line_opt && !word_opt && !char_opt) {
+		printf("%d %d %d", line_count, word_count, char_count);
+	}
+	printf(" %s\n", filename);
+
+	if (strcmp(filename, "") != 0) {
+		fclose(f);
+	}
 	return EXIT_SUCCESS;
 }
 
